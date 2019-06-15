041d55f4efea468d82b2a33b7eed0df3cde19d57
M0H314-hasani <m0h314.hasani@gmail.com>
2019-06-15 11:48:45 +0430

    Fix bug: one of the routes never used!

--- routes/api.php
+++ routes/api.php
@@ -22,7 +22,6 @@ Route::prefix('auth')->middleware('api')->group(function (){
     Route::post('login', 'AuthController@login');
     Route::post('logout', 'AuthController@logout');
     Route::post('refresh', 'AuthController@refresh');
-    Route::post('me', 'AuthController@me');
 });
 
 Route::prefix('user')->group(function (){
